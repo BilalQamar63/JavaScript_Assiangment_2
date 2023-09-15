fe2d356a189396abbe62fd2b35acf421f418724a
BilalQamar63 <143898156+BilalQamar63@users.noreply.github.com>
2023-09-15 17:21:01 +0500

    Second Assignment done

---
+++ JavaScript_Assignment_2/app.js
@@ -0,0 +1,436 @@
+// Chapter 9-11 ==== Q1======
+
+
+// var userCity = prompt("Please Enter your city name");
+// if(userCity ==="karachi"){
+//     alert("Welcome to city of lights")
+// }
+// else{
+//     alert("Come the city of lights")
+// }
+
+
+// Chapter 9-11===== Q2======
+
+// var userGender = prompt("Please Enter your Gender");
+// if(userGender==="male"){
+//     alert("Good morning Sir")
+// }
+
+// else if(userGender==="female")
+// {
+//     alert("Good morning Mam")
+// }
+
+// else{
+//     alert("Please Enter Correct spelling")
+// }
+
+
+// Chapter 9-11===Q3====
+
+// var trafficSignal = prompt("Enter Road Traffic color red, yellow,  green");
+// if(trafficSignal==="red"){
+//     alert("Must Stop")
+// }
+
+// else if(trafficSignal==="yellow"){
+//     alert("Ready To Move")
+// }
+
+// else if(trafficSignal==="green"){
+//     alert("Move Now")
+// }
+// else{
+//     alert("Enter Correct Spelling")
+// }
+
+
+// ====Chapter 9-11====Q4===
+
+
+// var fuelInput = +prompt("Enter How many litres fuel in your Car!!!!..")
+// if(fuelInput===1){
+//     alert("OK fuel filling is done")
+// }
+// else{
+//     alert("Please refill the fuel in you car...")
+// }
+
+
+// Chapter 9-11 ===Q5 (a)===
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// ******Yes message alert is displayed on Screen*****
+
+// Chapter 9-11 ===Q5 (b)===
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// ******No this message alert is does not displayed on Screen*****
+
+// Chapter 9-11 ===Q5 (c)===
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+
+
+// ******No this message alert is does not displayed on Screen*****
+
+// if (c === 13){
+//     alert("condition 2 is true");
+//     }
+    
+// ******Yes message alert is displayed on Screen*****
+
+
+    // if (++c < 14){
+    // alert("condition 3 is true");
+    // }
+
+
+// ******No this message alert is does not displayed on Screen*****
+
+    // if(c === 14){
+    // alert("condition 4 is true");
+    // }
+
+ // ******Yes message alert is displayed on Screen*****
+
+
+
+// Chapter 9-11 ===Q5 (d)===
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// ******Yes message alert is displayed on Screen*****
+
+// Chapter 9-11 ===Q5 (e)===
+
+// if (true){
+//     alert("True");
+//     }
+
+    // ******Yes message alert is displayed on Screen*****
+
+    // if (false){
+    // alert("False");
+    // }
+    
+// ******No this message alert is does not displayed on Screen*****
+
+
+// Chapter 9-11 ===Q5 (e)===
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+    
+    // ******Yes message alert is displayed on Screen*****
+
+// Chapter 9-11 ===Q6===
+
+    // var Subjectone = +prompt("Enter Marks Subject One")
+    // var Subjecttwo = +prompt("Enter Marks Subject two")
+    // var Subjectthree = +prompt("Enter Marks Subject three")
+    // var Obtainedmarks = (Subjectone + Subjecttwo + Subjectthree)
+    // var totalMarks = +prompt("Enter your totalMarks")
+    // var percentage=(Obtainedmarks/totalMarks*100)
+    // if(percentage >=80)
+    // {
+    //   document.write("TotalMarks:",totalMarks, "<br />")
+    //   document.write("ObtainedMarks:", Obtainedmarks, "<br />");
+    //   document.write(" Grade:A-one, <br />");
+    //   document.write(percentage, "<br />");
+    //   document.write ("You Need to Improve")
+    // }
+    // else if(70>=60){
+    //     document.write("TotalMarks:",totalMarks, "<br />");
+    //   document.write("ObtainedMarks:", Obtainedmarks, "<br />");
+    //   document.write(" Grade:A, <br />");
+    //   document.write(percentage, "<br />");
+    //   document.write ("You Need to Improve");
+    // }
+    // else if(60>=50){
+    //     document.write("TotalMarks:",totalMarks, "<br />");
+    //   document.write("ObtainedMarks:", Obtainedmarks, "<br />");
+    //   document.write(" Grade:B, <br />");
+    //   document.write(percentage, "<br />");
+    //   document.write ("You Need to Improve");
+    // }
+    // else if(50>=40){
+    //     document.write("TotalMarks:",totalMarks, "<br />");
+    //     document.write("ObtainedMarks:", Obtainedmarks, "<br />");
+    //     document.write(" Grade:C, <br />");
+    //     document.write(percentage, "<br />");
+    //     document.write ("You Need to Improve");
+    // }
+    // else if(40>=33){
+    //     document.write("TotalMarks:",totalMarks, "<br />");
+    //   document.write("ObtainedMarks:", Obtainedmarks, "<br />");
+    //   document.write(" Grade:D, <br />");
+    //   document.write(percentage, "<br />");
+    //   document.write ("You Need to Improve");
+    // }
+    // else if(33>=0){
+    //     document.write("TotalMarks:",totalMarks, "<br />");
+    //   document.write("ObtainedMarks:", Obtainedmarks, "<br />");
+    //   document.write(" Grade:E, <br />");
+    //   document.write(percentage, "<br />");
+    //   document.write ("You Need to Improve");
+    // }
+    // else{
+    //    document.write("Please Enter correct Statment")
+    // }
+    
+    // Chapter 9-11 ===Q7===
+
+    // var secretNumber = +prompt("Please Enter Any Number ")
+    // if(secretNumber===8){
+    //     alert("Bingo! Correct Answer");
+    // }
+    // else if(secretNumber===4){
+    //     alert("Bingo! Correct Answer");
+    // }
+    // else if(secretNumber===3){
+    //     alert("Bingo! Correct Answer");
+    // }
+    // else if(secretNumber===5){
+    //     alert("Bingo! Correct Answer");
+    // }
+    // else if(secretNumber===6){
+    //     alert("Bingo! Correct Answer");
+    // }
+    // else{
+    //     alert("Close enough to the correct answer");
+    // }
+
+    // Chapter 9-11 ===Q8===
+
+    // var divisibleNumber = +prompt();
+    // var divideNumber = divisibleNumber % 3
+    // if(divideNumber ===0){
+    //     alert("This number is Divide by 3")
+    // }
+    // else{
+    //     alert("Enter Any Odd Number");
+    // }
+
+    // chapter 9-11 =====Q9===
+
+    //  var oddNumber = +prompt("Enter Odd Number For Calculation");
+    // var oddCalculation = oddNumber % 3
+    // if(oddCalculation ===0){
+    //     alert("This number is Divide by 3")
+    // }
+    // else{
+    //     alert("this is primary number")
+    // }
+    
+//     var number = +prompt("Enter your number");
+
+// if (number % 2 === 0) {
+//   alert("even number");
+// } else if (number % 3 === 0) {
+//   alert("odd number");
+// } else {
+//   alert("neither ever nor odd number it is primary number");
+// }
+
+    // Chapter 9-11 =====Q10====
+
+    // var temprature = +prompt("Enter Temprature value");
+    // if(temprature>=40){
+    //     alert("It is too hot Outside");
+    // }
+    // else if(30>=20){
+    //     alert("The Weather  today is Normal");
+    // }
+    // else if(20>=10){
+    //     alert("Today's Weather is  Cool");
+    // }
+    // else if(10>=0){
+    //     alert("OMG! Today's Weather is Soo Cool ");
+    // }
+
+     // Chapter 9-11 =====Q11====
+
+    //  var firstNumber = +prompt("enter first Number");
+    //  var operator = prompt("enter your operator");
+    //  var  secondNumber= +prompt("Enter second Number");
+     
+    //  if (operator === "+")
+    //  {
+    //    alert(firstNumber + secondNumber);
+    // }
+    //  else if (operator === "-")
+    //   {
+    //    alert(firstNumber - secondNumber);
+    // }
+    //     else if (operator === "*")
+    //      {
+    //    alert(firstNumber * secondNumber);
+    // }
+    //     else if (operator === "/") 
+    //     {
+    //    alert(firstNumber / secondNumber);
+    // }
+    // else if(operator === "%"){
+    //     alert(firstNumber % secondNumber)
+    // }
+
+
+     // Chapter 12-13 =====(Q1)===
+
+    // Prompt the user for input
+// var userInput = prompt("Enter a character:");
+
+// // Check if the input is a single character
+// if (userInput.length === 1) {
+//   var charCode = userInput.charCodeAt(0);
+
+//   // Check if the character is a number
+//   if (charCode >= 48 && charCode <= 57) {
+//     console.log("The input is a number.");
+//   }
+//   // Check if the character is an uppercase letter
+//   else if (charCode >= 65 && charCode <= 90) {
+//     console.log("The input is an uppercase letter.");
+//   }
+//   // Check if the character is a lowercase letter
+//   else if (charCode >= 97 && charCode <= 122) {
+//     console.log("The input is a lowercase letter.");
+//   }
+//   // If it doesn't match any of the above conditions, it's something else
+//   else {
+//     console.log("The input is not a number, uppercase letter, or lowercase letter.");
+//   }
+// }
+// // If the input is not a single character
+// else {
+//   console.log("Please enter a single character.");
+// }
+
+
+
+// Chapter 12-13 =====(Q2)===
+
+// Prompt the user for two integers
+// var numbers1 = parseInt(prompt("Enter the first integer:"));
+// var numbers2 = parseInt(prompt("Enter the second integer:"));
+
+// // Check if both numbers are equal
+// if (numbers1 === numbers2) {
+//   console.log("Both numbers are equal.");
+// } else if (numbers1 > numbers2) {
+//   console.log("The larger number is: " + numbers1);
+// } else {
+//   console.log("The larger number is: " + numbers2);
+// }
+
+// Chapter 12-13 =====(Q3)===
+
+
+// Prompt the user for input
+// var userInput = parseFloat(prompt("Enter a number:"));
+
+// // Check if the input is a number
+// if (userInput) {
+//   if (userInput > 0) {
+//     console.log("The number is positive.");
+//   } else if(userInput < 0) {
+//     console.log("The number is negative.");
+//   } 
+// } else {
+//   console.log("Invalid input. Please enter a valid number.");
+// }
+
+
+// Chapter 12-13 =====(Q4)===
+
+// var vowel = prompt("Enter any alphabet")
+// if(vowel==="a"){
+//     alert(true)
+// }
+// else if(vowel==="e"){
+//     alert(true)
+// }
+// else if(vowel==="i"){
+//     alert(true)
+// }
+// else if(vowel==="o"){
+//     alert(true)
+// }
+// else if(vowel==="u"){
+//     alert(true)
+// }
+// else{
+//     alert(false)
+// }
+
+// Chapter 12-13 =====(Q5)===
+
+// var correctPassword = "my Secret Password";
+// var userPassword = prompt("Enter your password:");
+// if(userPassword=== null || userPassword===""){
+//     alert("Please enter your password.");
+// }
+// else{
+//     if(userPassword===correctPassword){
+//         alert("Correct! The password you entered matches the original password.")
+//     }
+//     else{
+//         alert("Incorrect password.")
+//     }
+// }
+
+// Chapter 12-13 =====(Q6)===
+
+// var greeting="Good day"
+// var hour =prompt() ;
+// if (hour <= 18)
+// {
+// alert("Good day");
+// }
+// else{
+// alert("Good evening")
+// }
+
+// Chapter 12-13 =====(Q7)===
+
+// // Ask the user to enter the time in 24-hour clock format (e.g., 1900)
+// var userTime = parseInt(prompt("Enter the time in 24-hour clock format (e.g., 1900):"));
+
+// // Check if the user entered a valid number
+// if (userTime) {
+//   if (userTime >= 0 && userTime <= 2359) {
+//     if (userTime >= 0 && userTime < 1200) {
+//       alert("Good morning!");
+//     } else if (userTime >= 1200 && userTime < 1700) {
+//       alert("Good afternoon!");
+//     } else if (userTime >= 1700 && userTime < 2100) {
+//       alert("Good evening!");
+//     } else {
+//       alert("Good night!");
+//     }
+//   } else {
+//     alert("Invalid input. Please enter a valid time between 0000 and 2359.");
+//   }
+// } else {
+//   alert("Invalid input. Please enter a valid number.");
+// }
